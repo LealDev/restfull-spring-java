96579ca30ac422c0229b4d803dad942b46a6459a
LealDev <lealmaster0111@gmail.com>
2024-11-24 16:16:50 -0300

    First endpoint test

---
+++ src/main/java/com/lealdev/restfull_spring_java/Greeting.java
@@ -0,0 +1,19 @@
+package com.lealdev.restfull_spring_java;
+
+public class Greeting {
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
--- src/main/java/com/lealdev/restfull_spring_java/Startup.java
+++ src/main/java/com/lealdev/restfull_spring_java/Startup.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestfullSpringJavaApplication {
+public class Startup {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RestfullSpringJavaApplication.class, args);
+		SpringApplication.run(Startup.class, args);
 	}
 
 }

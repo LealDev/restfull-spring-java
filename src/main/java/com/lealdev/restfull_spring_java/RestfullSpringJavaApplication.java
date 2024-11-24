53291097275ff4ec29b09ceb79c9e2fa4a388530
LealDev <lealmaster0111@gmail.com>
2024-11-24 12:00:43 -0300

    first commit

---
+++ src/main/java/com/lealdev/restfull_spring_java/RestfullSpringJavaApplication.java
@@ -0,0 +1,13 @@
+package com.lealdev.restfull_spring_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfullSpringJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfullSpringJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lealdev/restfull_spring_java/RestfullSpringJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lealdev.restfull_spring_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfullSpringJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
